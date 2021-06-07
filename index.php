534f7fc1a7a890fe76e572cd0520268c272d5ac3
ねこ丸 / nekomaru <64449857+magicgang@users.noreply.github.com>
2021-06-07 12:20:08 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,3 +4,11 @@ header('Content-type: text/plain');
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, 'https://api.mozambiquehe.re/maprotation?version=2&auth=yAu1KcMj351O0aZZkjY1');
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
+
+print_r($result);
